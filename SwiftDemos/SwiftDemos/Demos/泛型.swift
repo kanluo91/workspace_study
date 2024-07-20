2e0032f96e98c1ea75e9287fe34fb5e8663272ef
kan.luo <kan.luo@zeekrlife.com>
2024-07-20 11:36:12 +0800

    初始化swift工程

---
+++ SwiftDemos/SwiftDemos/Demos/泛型.swift
@@ -0,0 +1,12 @@
+//
+//  泛型.swift
+//  SwiftDemos
+//
+//  Created by luokan on 2024/7/20.
+//
+
+import Foundation
+
+func 泛型_test1(){
+    
+}
---
+++ SwiftDemos/SwiftDemos/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  SwiftDemos
+//
+//  Created by luokan on 2024/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var datas: [String] {
+        return ["泛型","多线程"]
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        
+        let tableV = UITableView(frame: self.view.frame)
+        tableV.delegate = self
+        tableV.dataSource = self
+        self.view.addSubview(tableV)
+    }
+
+}
+
+extension ViewController : UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.datas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell(style: .default, reuseIdentifier: "12313")
+        cell.textLabel?.text = self.datas[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        泛型_test1()
+        
+    }
+    
+}
