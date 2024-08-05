cffbc09ebd094cce9b9b550e90867b9bb83af03e
kan.luo <kan.luo@zeekrlife.com>
2024-08-06 02:26:21 +0800

    生成jar

---
+++ android/mylibrary2/src/main/java/com/example/mylibrary2/OneClass.java
@@ -0,0 +1,11 @@
+package com.example.mylibrary2;
+
+import android.util.Log;
+
+public class OneClass {
+
+    public void sayhello(String msg){
+        Log.i("我的类库",msg);
+    }
+
+}
---
+++ android/mylibrary3/src/main/java/com/example/mylibrary3/OneClass.java
@@ -0,0 +1,11 @@
+package com.example.mylibrary3;
+
+import android.util.Log;
+
+public class OneClass {
+
+    public void sayhello(String msg){
+        Log.i("我的类库",msg);
+    }
+
+}
